b631247458e856308bf341bca7401a84bd741975
Kata12344 <kataszundi@gmail.com>
2022-05-19 14:52:20 +0200

    teszt futtatása

--- src/torpedo_valtozat2/Hajo.java
+++ src/torpedo_valtozat2/Hajo.java
@@ -3,6 +3,7 @@ package torpedo_valtozat2;
 
 public class Hajo {
     static int[] pozicio = new int[3];
+    
     public static String talalat(int poz){
         return "";
     }
--- src/torpedo_valtozat2/TorpedoTeszt.java
+++ src/torpedo_valtozat2/TorpedoTeszt.java
@@ -4,6 +4,10 @@ package torpedo_valtozat2;
 public class TorpedoTeszt {
     public static void main(String[] args) {
         
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(4);
+        assert t.equals("talált"):"nem jó a találat ellenőrzése";
+        
     }
     public static String tesztLoves(int poz){
         return " ";
