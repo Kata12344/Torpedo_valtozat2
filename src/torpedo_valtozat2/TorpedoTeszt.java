6c033c3fee602dadb859e9c5720e41c18fe0690d
Kata12344 <kataszundi@gmail.com>
2022-05-19 14:41:56 +0200

    TorpedoTeszt osztály létrehozása

--- src/torpedo_valtozat2/TorpedoTeszt.java
+++ src/torpedo_valtozat2/TorpedoTeszt.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package torpedo_valtozat2;
 
-/**
- *
- * @author User
- */
 public class TorpedoTeszt {
-    
+    public static void main(String[] args) {
+        
+    }
+    public static String tesztLoves(int poz){
+        return " ";
+    }
 }
